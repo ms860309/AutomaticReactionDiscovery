4068004a040277dde593ebcdd1a702c6c5aa91a0
Jianyi <ms860309@gmail.com>
2020-04-07 03:56:18 +0800

    energy done

---
+++ database/checker.py
@@ -0,0 +1,120 @@
+# connect to database
+
+# search entries with status "job_launched" or "job_running"
+# check the job pbs-status, e.g., qstat -f "job_id"
+# 1. Invalid job id specified: means the job is off queue
+#    1.1 check job folder if input.log file is there
+#			Yes: job already finished
+#			No:  job is aborted before even calculation start: job_aborted
+#    1.2 if finished, check "Normal termination" key words should appear twice
+#			Yes: job is converged
+#			No:  job fails convergence (possibly need more wall-time): job_failed_convergence
+#    1.3 if converged, check isomorphism between input molecule and output molecule
+#			Yes: job is success: job_success
+#			No:  job fails isomorphism (possibly need tweak on initial structure): job_failed_isomorphism
+# 2. output string starts with e.g, "JobId=5037088"
+#    2.1 check JobState
+#		RUNNING: running: job_running
+#		else:   still job_launched 
+
+from connect import Connector
+import subprocess
+import os
+from os import path
+import sys
+
+def select_energy_target():
+	"""
+	This method is to inform job checker which targets 
+	to check, which need meet one requirement:
+	1. status is job_launched or job_running
+	Returns a list of targe
+	"""
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+	reg_query = {"energy_status":
+				{ "$in": 
+					["job_launched", "job_running"] 
+				}
+			}
+    targets = list(collect.find(reg_query))
+
+    return targets
+
+def check_energy_status(job_id):
+	"""
+	This method checks slurm status of a job given job_id
+	Returns off_queue or job_launched or job_running
+	"""
+
+	commands = ['qstat', '-f', job_id]
+	process = subprocess.Popen(commands,
+								stdout=subprocess.PIPE,
+								stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+
+	if "Unknown Job Id" in stderr.decode():
+		return "off_queue"
+
+	assert "JobId={0}".format(job_id) in stdout, 'PBS cannot show details for job_id {0}'.format(job_id)
+ 
+    # in pbs stdout is byte, so we need to decode it at first.
+    stdout = stdout.decode().strip().split()
+    idx = stdout.index('job_state')
+    if stdout[idx+2] == 'R':
+        return "job_running"
+    else:
+        return "job_launched"
+    
+def check_energy_content_status(path):
+
+    energy_path = os.path.join(path, "energy.out")
+    if not path.exists(energy_path):
+        return "job_aborted"
+    else:
+        with open(energy_path, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.strip().startswith('SCF   energy in the final basis set'):
+                    SCF_Energy = line.split()[-1]
+                    energy = float(SCF_Energy)*627.5095
+        return "job_success", energy
+
+def check_energy_jobs():
+	"""
+	This method checks job with following steps:
+	1. select jobs to check
+	2. check the job pbs-status, e.g., qstat -f "job_id"
+	3. check job content
+	4s. update with new status
+	"""
+	# 1. select jobs to check
+	targets = select_energy_target()
+ 
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+    
+	# 2. check the job slurm-status
+	for target in targets:
+		job_id = target['energy_jobid']
+		# 2. check the job slurm_status
+		new_status = check_energy_status(job_id)
+		if new_status == "off_queue":
+			# 3. check job content
+			new_status, energy = check_energy_content_status(target['path'])
+		
+		# 4. check with original status which
+		# should be job_launched or job_running
+		# if any difference update status
+		orig_status = target['energy_status']
+		if orig_status != new_status:
+			update_field = {
+					'energy_status': new_status,
+                    'SCF_energy':energy
+			}
+
+			collect.update_one(target, {"$set": update_field}, True)
+
+check_energy_jobs()
---
+++ database/launch.py
@@ -0,0 +1,142 @@
+from connect import Connector
+import subprocess
+import os
+from os import path
+import sys
+sys.path.append(path.join(path.dirname( path.dirname( path.abspath(__file__))),'script'))
+# gsm is in script dir for launch gsm
+
+"""
+Submmit energy calculation job
+1. select unrun job
+2. push unrun job to qchem
+3. update status "job_launched"
+"""
+def select_calE_target():
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+    reg_query = {"energy_status":"job_unrun"}
+    targets = list(collect.find(reg_query))
+
+    selected_targets = []
+    for target in targets:
+        dir_path = target['path']
+        selected_targets.append(dir_path)
+
+    return selected_targets
+
+def launch_energy_jobs():
+    targets = select_calE_target()
+    
+    for target in targets:
+        if os.path.exists(target):
+            os.chdir(target)
+        subfile = create_energy_sub_file(target)
+        try:
+            cmd = 'qsub {}'.format(subfile)
+            process = subprocess.Popen([cmd],
+								stdout=subprocess.PIPE,
+								stderr=subprocess.PIPE, shell = True)
+            stdout, stderr = process.communicate()
+        except:
+            continue
+        # get job id from stdout, e.g., "106849.h81"
+        job_id = stdout.decode()
+        # update status job_launched
+        update_energy_status(target, job_id)
+        
+def create_energy_sub_file(path, ncpus = 1, mpiprocs = 1):
+    subfile = os.path.join(path, 'calculate_energy.job')
+    
+    shell = '#!/usr/bin/bash'
+    pbs_setting = '#PBS -l select=1:ncpus={}:mpiprocs={}'.format(ncpus, mpiprocs)
+    target_path = 'cd {}'.format(path)
+    nes1 = 'source ~/.bashrc\n conda activate rmg3'
+    nes2 = 'export QCSCRATCH=/tmp/ypli/$PBS_JOBID'
+    nes3 = 'mkdir -p $QCSCRATCH'
+    nes4 = 'qchem -nt 2 energy.in energy.out'
+    nes5 = 'rm -r $QCSCRATCH'
+    with open(subfile, 'w') as f:
+        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}').format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5)
+    return subfile
+    
+def update_energy_status(target, job_id):
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+    reg_query = {"path":target}
+    update_field = {"energy_status":"job_launched", "energy_jobid":job_id}
+    collect.update_one(reg_query, {"$set": update_field}, True)
+    
+"""
+Submmit SSM calculation job
+1. select unrun job
+2. push unrun job to qchem
+3. update status "job_launched"
+"""
+    
+def select_ssm_target():
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+    reg_query = {"ssm_status":"job_unrun"}
+    targets = list(collect.find(reg_query))
+
+    selected_targets = []
+    for target in targets:
+        dir_path = target['path']
+        selected_targets.append(dir_path)
+
+    return selected_targets
+
+def launch_ssm_jobs():
+    targets = select_ssm_target()
+    
+    for target in targets:
+        if os.path.exists(target):
+            os.chdir(target)
+            
+        subfile = create_ssm_sub_file(target)
+        try:
+            cmd = 'qsub {}'.format(subfile)
+            process = subprocess.Popen([cmd],
+								stdout=subprocess.PIPE,
+								stderr=subprocess.PIPE, shell = True)
+            stdout, stderr = process.communicate()
+        except:
+            continue
+        # get job id from stdout, e.g., "106849.h81"
+        job_id = stdout.decode()
+        # update status job_launched
+        update_ssm_status(target, job_id)
+        
+def create_ssm_sub_file(path, ncpus = 1, mpiprocs = 1):
+    subfile = path.join(path, 'calculate_ssm.job')
+    
+    xyz_file = path.join(path, 'reactant.xyz')
+    isomers = path.join(path, 'add_bonds.txt')
+    lot_inp_file = path.join(path.join(path.dirname( path.dirname( path.abspath(__file__))),'submmit_required'), qstart)
+    
+    shell = '#!/usr/bin/bash'
+    pbs_setting = '#PBS -l select=1:ncpus={}:mpiprocs={}'.format(ncpus, mpiprocs)
+    target_path = 'cd {}'.format(path)
+    nes1 = nes1 = 'source ~/.bashrc\n conda activate rmg3'
+    nes2 = 'export QCSCRATCH=/tmp/ypli/$PBS_JOBID'
+    nes3 = 'mkdir -p $QCSCRATCH'
+    nes4 = 'gsm -xyzfile {} -mode SE_GSM -package QChem -isomers {} -lot_inp_file {}'.format(xyz_file, isomers, lot_inp_file)
+    nes5 = 'rm -r $QCSCRATCH'
+    with open(subfile, 'w') as f:
+        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}').format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5)
+    return subfile
+    
+def update_ssm_status(target, job_id):
+    remote_client = Connector().client
+    db = remote_client['network']
+    collect = db['molecules']
+    reg_query = {"path":target}
+    update_field = {"ssm_status":"job_launched", "ssm_jobid":job_id}
+    collect.update_one(reg_query, {"$set": update_field}, True)
+    
+launch_energy_jobs()
+launch_ssm_jobs()
