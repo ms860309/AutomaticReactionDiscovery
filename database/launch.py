08a885145c6f0702102ff7ad2bc2b26ad78c7f9a
Jianyi <ms860309@gmail.com>
2020-04-22 01:49:12 +0800

    fix database job launch

--- database/launch.py
+++ database/launch.py
@@ -28,9 +28,15 @@ def launch_energy_jobs():
     targets = select_calE_target()
     
     for target in targets:
-        if os.path.exists(target):
-            os.chdir(target)
-        subfile = create_energy_sub_file(target)
+        Energy_dir_path = path.join(target, 'Energy')
+        if os.path.exists(Energy_dir_path):
+            os.chdir(Energy_dir_path)
+        else:
+            os.mkdir(Energy_dir_path)
+            os.chdir(Energy_dir_path)
+            
+        subfile = create_energy_sub_file(target, Energy_dir_path)
+        
         cmd = 'qsub {}'.format(subfile)
         process = subprocess.Popen([cmd],
                             stdout=subprocess.PIPE,
@@ -41,19 +47,19 @@ def launch_energy_jobs():
         # update status job_launched
         update_energy_status(target, job_id)
         
-def create_energy_sub_file(path, ncpus = 1, mpiprocs = 1):
-    subfile = os.path.join(path, 'calculate_energy.job')
+def create_energy_sub_file(path, Energy_dir_path, ncpus = 1, mpiprocs = 1):
+    subfile = os.path.join(Energy_dir_path, 'calculate_energy.job')
     
     shell = '#!/usr/bin/bash'
     pbs_setting = '#PBS -l select=1:ncpus={}:mpiprocs={}'.format(ncpus, mpiprocs)
-    target_path = 'cd {}'.format(path)
-    nes1 = 'source ~/.bashrc\n conda activate rmg3'
+    target_path = 'cd {}'.format(Energy_dir_path)
+    nes1 = 'source ~/.bashrc\nconda activate rmg3'
     nes2 = 'export QCSCRATCH=/tmp/ypli/$PBS_JOBID'
     nes3 = 'mkdir -p $QCSCRATCH'
     nes4 = 'qchem -nt 2 energy.in energy.out'
     nes5 = 'rm -r $QCSCRATCH'
     with open(subfile, 'w') as f:
-        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}').format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5)
+        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5))
     return subfile
     
 def update_energy_status(target, job_id):
@@ -73,7 +79,6 @@ def select_ssm_target():
     collect = db['molecules']
     reg_query = {"ssm_status":"job_unrun"}
     targets = list(collect.find(reg_query))
-
     selected_targets = []
     for target in targets:
         dir_path = target['path']
@@ -85,10 +90,14 @@ def launch_ssm_jobs():
     targets = select_ssm_target()
     
     for target in targets:
-        if os.path.exists(target):
-            os.chdir(target)
+        SSM_dir_path = path.join(target, 'SSM')
+        if os.path.exists(SSM_dir_path):
+            os.chdir(SSM_dir_path)
+        else:
+            os.mkdir(SSM_dir_path)
+            os.chdir(SSM_dir_path)
             
-        subfile = create_ssm_sub_file(target)
+        subfile = create_ssm_sub_file(target, SSM_dir_path)
         cmd = 'qsub {}'.format(subfile)
         process = subprocess.Popen([cmd],
                             stdout=subprocess.PIPE,
@@ -99,23 +108,22 @@ def launch_ssm_jobs():
         # update status job_launched
         update_ssm_status(target, job_id)
         
-def create_ssm_sub_file(path, ncpus = 1, mpiprocs = 1):
-    subfile = path.join(path, 'calculate_ssm.job')
-    
-    xyz_file = path.join(path, 'reactant.xyz')
-    isomers = path.join(path, 'add_bonds.txt')
-    lot_inp_file = path.join(path.join(path.dirname( path.dirname( path.abspath(__file__))),'submmit_required'), qstart)
+def create_ssm_sub_file(dir_path, SSM_dir_path, ncpus = 1, mpiprocs = 1):
+    subfile = path.join(SSM_dir_path, 'calculate_ssm.job')
+    xyz_file = path.join(dir_path, 'reactant.xyz')
+    isomers = path.join(dir_path, 'add_bonds.txt')
+    lot_inp_file = path.join(path.join(path.dirname( path.dirname( path.abspath(__file__))),'submmit_required'), 'qstart')
     
     shell = '#!/usr/bin/bash'
     pbs_setting = '#PBS -l select=1:ncpus={}:mpiprocs={}'.format(ncpus, mpiprocs)
-    target_path = 'cd {}'.format(path)
-    nes1 = nes1 = 'source ~/.bashrc\n conda activate rmg3'
+    target_path = 'cd {}'.format(SSM_dir_path)
+    nes1 = nes1 = 'source ~/.bashrc\nconda activate rmg3'
     nes2 = 'export QCSCRATCH=/tmp/ypli/$PBS_JOBID'
     nes3 = 'mkdir -p $QCSCRATCH'
     nes4 = 'gsm -xyzfile {} -mode SE_GSM -package QChem -isomers {} -lot_inp_file {}'.format(xyz_file, isomers, lot_inp_file)
     nes5 = 'rm -r $QCSCRATCH'
     with open(subfile, 'w') as f:
-        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}').format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5)
+        f.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(shell, pbs_setting, target_path, nes1, nes2, nes3, nes4, nes5))
     return subfile
     
 def update_ssm_status(target, job_id):
@@ -124,5 +132,5 @@ def update_ssm_status(target, job_id):
     update_field = {"ssm_status":"job_launched", "ssm_jobid":job_id}
     collect.update_one(reg_query, {"$set": update_field}, True)
     
-launch_energy_jobs()
+#launch_energy_jobs()
 launch_ssm_jobs()
