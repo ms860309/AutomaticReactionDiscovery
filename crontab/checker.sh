bcdc6e02710ee175b84002c50adfbe71b85dc810
Jianyi <ms860309@gmail.com>
2020-04-22 20:15:21 +0800

    fix crontab sh file

--- crontab/checker.sh
+++ crontab/checker.sh
@@ -1,2 +1,5 @@
 echo $(date +%Y-%m-%d:%H:%M:%S)
+source ~/.bashrc
+conda activate rmg3
 python /home/hpc/ypli/jianyi/test_pygsm/AutomaticReactionDiscovery/database/checker.py
+conda deactivate
--- crontab/launch.sh
+++ crontab/launch.sh
@@ -1,2 +1,5 @@
-echo $(date +%Y-%m-%d:%H:%M:%S) 
+echo $(date +%Y-%m-%d:%H:%M:%S)
+source ~/.bashrc
+conda activate rmg3
 python /home/hpc/ypli/jianyi/test_pygsm/AutomaticReactionDiscovery/database/launch.py
+conda deactivate
