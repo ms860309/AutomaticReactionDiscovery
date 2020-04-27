ebaaf8e28da1a0d25d0ff519b36a213fa3bf0265
Jianyi <ms860309@gmail.com>
2020-04-28 02:37:28 +0800

    add inchi_key check and molecules pic of first generation

--- script/molDrawer.py
+++ script/molDrawer.py
@@ -15,10 +15,34 @@ def readXYZ(path):
 def molDrawer():
     dirs = os.listdir('/mnt/d/reactions')
     for i in dirs:
-        dir_path = os.path.join(os.path.join('/mnt/d/reactions', i), 'product.xyz')
+        dir_path = os.path.join(os.path.join('/mnt/d/reactions', i), 'ssm_product.xyz')
         OBMol = readXYZ(dir_path)
         rmg_mol = toRMGmol(OBMol)
         _path = '/mnt/d/molecules/{}.png'.format(i)
         MoleculeDrawer().draw(rmg_mol, file_format='png', target=_path)
         
-molDrawer()
+def generate_ssm_product_xyz():
+    opt_file = []
+    reactions_path = '/mnt/d/reactions'
+    reactions_path_list = os.listdir(reactions_path)
+    for i in reactions_path_list:
+        target_path = os.path.join(reactions_path, i)
+        path_list=os.listdir(os.path.join(target_path, 'SSM'))
+        path_list.sort()
+        for filename in path_list:
+            if filename.startswith('opt'):
+                opt_file.append(filename)
+        product_xyz = os.path.join(os.path.join(target_path, 'SSM'), opt_file[-1])
+        with open(product_xyz, 'r') as f:
+            lines = f.readlines()
+            for i in reversed(lines):
+                a = i.split()
+                if len(a) == 1:
+                    idx = lines.index(i)
+                    break
+        parent_ssm_product_path  = os.path.join(os.path.abspath(os.path.join(os.path.join(target_path, 'SSM'), '../')), 'ssm_product.xyz')
+        with open(parent_ssm_product_path,'w') as q:
+            q.write('{}\n{}'.format(lines[idx-1], ''.join(lines[idx+1:])))
+                
+#molDrawer()
+#generate_ssm_product_xyz()
