6800791870a804ff1f87308f286aca109b810415
Jianyi <ms860309@gmail.com>
2020-04-27 13:39:35 +0800

    fix

--- crontab/checker.sh
+++ crontab/checker.sh
@@ -1,6 +1,5 @@
-#/usr/bin/bash
-echo $(date +%Y-%m-%d:%H:%M:%S)
 source ~/.bashrc
 conda activate rmg3
 python /home/hpc/ypli/jianyi/test_pygsm/AutomaticReactionDiscovery/database/checker.py
 conda deactivate
+echo $(date +%Y-%m-%d:%H:%M:%S)
